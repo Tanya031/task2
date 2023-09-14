0483fd8752cb6d87858013314bcfdc294e49112b
Tanya031 <76044305+Tanya031@users.noreply.github.com>
2023-09-14 16:32:51 +0530

    Add files via upload

---
+++ task2.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+void cal(double num1, double num2)
+{
+    char choice;
+    double result;
+    cout
+        << "Enter the operation to be performed(+,-,*,/,%) : ";
+    cin >> choice;
+    switch (choice)
+    {
+    case '+':
+        result = num1 + num2;
+        break;
+    case '-':
+        result = num1 - num2;
+        break;
+    case '*':
+        result = num1 * num2;
+        break;
+    case '/':
+        result = num1 / num2;
+        break;
+    case '%':
+        result = double(int(num1) % int(num2));
+        break;
+    }
+    cout << num1 << choice << num2 << "=" << result << endl;
+    return;
+}
+int main()
+{
+    double num1, num2;
+    cout << "Enter the 2 two numbers : " << endl;
+    cin >> num1 >> num2;
+    cal(num1, num2);
+}
